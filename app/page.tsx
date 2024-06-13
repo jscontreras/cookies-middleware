bbcce921005b0a0eea08f3575a356f644aa2cd8d
Sebastian Contreras <js.contreras@outlook.com>
2024-06-13 10:32:48 -0500

    Reading cookies with middleware.

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,12 @@
-/** Add your relevant code here for the issue to reproduce */
+import { cookies } from "next/headers";
+
+
 export default function Home() {
-  return <h1>Hello World (M)</h1>;
+  // Reading query from Server
+  const cookieStorage = cookies();
+  const device = cookieStorage.get('device') || { value:'Non-Detected'};
+  return<>
+    <h1>Hello World (M)</h1>
+    <p>Device Cookie (Read From Server): {`${device?.value}`}</p>
+  </>;
 }
