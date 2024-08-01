b893aad9cdb7785e40227c13da33d4fcd57352ce
Sebastian Contreras <js.contreras@outlook.com>
2024-08-01 17:33:57 -0500

    Cookies API.

---
+++ app/api/print-headers/route.ts
@@ -0,0 +1,17 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(request: NextRequest) {
+  // Convert headers to an object
+  const headers: any = {};
+  request.headers.forEach((value, key) => {
+    headers[key] = value;
+  });
+  delete headers['x-vercel-protection-bypass'];
+  delete headers['x-vercel-sc-headers'];
+  delete headers['x-vercel-proxy-signature'];
+  return NextResponse.json({
+    now: Date.now(),
+    origin: 'https://cookies-middleware.vercel.app/',
+    headers: headers,
+  });
+}
